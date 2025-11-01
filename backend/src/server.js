067f5791a7255b30969ff37ccfc9247cf6c345b9
sohail-RM2004 <m.sohailrehan@gmail.com>
2025-11-01 22:58:56 +0530

    Fix: Downgrade Express to 4.x for stable routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,7 +38,7 @@ const frontendPath = path.join(__dirname, '../../frontend/dist');
 app.use(express.static(frontendPath));
 
 // Catch all handler for React Router
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
